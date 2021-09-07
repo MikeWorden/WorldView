31d12941d20592651f2bb75479babb83bd8d77ea
Mike Worden <michael.worden@icloud.com>
2021-09-07 13:59:09 -0600

    Added CAGradientLayer to view

--- WorldView/ViewController.swift
+++ WorldView/ViewController.swift
@@ -8,6 +8,26 @@
 import UIKit
 
 class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let firstFrame=CGRect(x: 160, y: 240, width: 100, height: 150)
+        let firstView = UIView(frame: firstFrame)
+        firstView.backgroundColor = UIColor.blue
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = view.bounds
+        gradientLayer.colors  = [UIColor.purple.cgColor,
+                                 UIColor.blue.cgColor,
+                                 UIColor.green.cgColor,
+                                 UIColor.yellow.cgColor,
+                                 UIColor.orange.cgColor,
+                                 UIColor.red.cgColor
+                                 ]
+        view.layer.insertSublayer(gradientLayer, at:0)
+        
+        view.addSubview(firstView)
+        
+        view.sendSubviewToBack(firstView)
+    }
 
     
 }
