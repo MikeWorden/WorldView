d4684da1e626cc396c468eec1910d31550d6eb6c
Mike Worden <michael.worden@icloud.com>
2021-09-16 20:28:07 -0600

    Added SegmentControl

--- WorldView/ConversionViewController.swift
+++ WorldView/ConversionViewController.swift
@@ -8,13 +8,15 @@
 import UIKit
 
 class ConversionViewController: UIViewController {
+    
+    	
     override func viewDidLoad() {
         super.viewDidLoad()
         print( "ConversionViewController loaded its view.")
         let firstFrame=CGRect(x: 160, y: 240, width: 100, height: 150)
         let firstView = UIView(frame: firstFrame)
         firstView.backgroundColor = UIColor.blue
-        let gradientLayer = CAGradientLayer()
+        /*let gradientLayer = CAGradientLayer()
         gradientLayer.frame = view.bounds
         gradientLayer.colors  = [UIColor.purple.cgColor,
                                  UIColor.blue.cgColor,
@@ -27,7 +29,25 @@ class ConversionViewController: UIViewController {
         
         view.addSubview(firstView)
         
-        view.sendSubviewToBack(firstView)
+        view.sendSubviewToBack(firstView)*/
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        let colorArray = [UIColor.purple,
+                          UIColor.blue,
+                          UIColor.green,
+                          UIColor.yellow,
+                          UIColor.orange,
+                          UIColor.red
+                          ]
+        var colorIndex: Int = 0
+        print ("Color Array has " + colorArray.count.description + " elements.")
+        colorIndex = Int.random(in: 0..<colorArray.count)
+        print("ColorIndex is \(colorIndex)")
+        print()
+        view.backgroundColor = colorArray[colorIndex]
+        
     }
 
     
