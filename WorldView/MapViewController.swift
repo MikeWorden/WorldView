48a2fbe20098e96d0f4bb7aaf8f533224e741e9c
Mike Worden <michael.worden@icloud.com>
2021-09-16 20:27:30 -0600

    Added SegmentControl..

--- WorldView/MapViewController.swift
+++ WorldView/MapViewController.swift
@@ -6,10 +6,28 @@
 //
 
 import UIKit
+import MapKit
 
 class MapViewController:  UIViewController {
+    var mapView:  MKMapView!
+    
+    override func loadView() {
+        mapView = MKMapView()
+        view = mapView
+        
+        let segmentedControl = UISegmentedControl(items:  ["Standard", "Hybrid", "Satellite"])
+        segmentedControl.backgroundColor = UIColor.systemBackground
+        segmentedControl.selectedSegmentIndex = 0
+        segmentedControl.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(segmentedControl)
+        
+        
+        
+    }
     override func viewDidLoad() {
+        super.viewDidLoad()
         print ("MapViewController laoded its view.")
+
     }
     
 }
