4239bd7e47e8bc86584905e61685ffec785e2969
Mike Worden <michael.worden@icloud.com>
2021-09-16 20:53:30 -0600

    Added programmatic constraints.

--- WorldView/MapViewController.swift
+++ WorldView/MapViewController.swift
@@ -17,16 +17,31 @@ class MapViewController:  UIViewController {
         
         let segmentedControl = UISegmentedControl(items:  ["Standard", "Hybrid", "Satellite"])
         segmentedControl.backgroundColor = UIColor.systemBackground
-        segmentedControl.selectedSegmentIndex = 0
+        segmentedControl.selectedSegmentIndex = 2
         segmentedControl.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(segmentedControl)
+        
+        //        let topConstraint = segmentedControl.topAnchor.constraint(equalTo: view.topAnchor)
+        let topConstraint = segmentedControl.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 0)
+
+        //let leadingConstraint = segmentedControl.leadingAnchor.constraint(equalTo: view.leadingAnchor)
+        //let trailingConstraint = segmentedControl.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        let margins = view.layoutMarginsGuide
+        let leadingConstraint = segmentedControl.leadingAnchor.constraint(equalTo: margins.leadingAnchor)
+        let trailingConstraint = segmentedControl.trailingAnchor.constraint(equalTo: margins.trailingAnchor)
+        
+        
+        
+        topConstraint.isActive = true
+        leadingConstraint.isActive = true
+        trailingConstraint.isActive = true
         
         
         
     }
     override func viewDidLoad() {
         super.viewDidLoad()
-        print ("MapViewController laoded its view.")
+        print ("MapViewController loaded its view.")
 
     }
     
